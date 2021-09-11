fcca9415ffb568e7036352a899edd6765632eb97
anushaakotha <90036480+anushaakotha@users.noreply.github.com>
2021-09-11 16:08:29 +0530

    Add files via upload

---
+++ src/main/java/com/project/group16/orderDetails/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.project.group16.orderDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+}
